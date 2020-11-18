ab10cb75a63a25c2bfdaa49f4f0af474f7736e74
Luis Ramirez <sdaewa22@gmail.com>
2020-11-18 13:55:58 +0000

    yargs.command

--- app.js
+++ app.js
@@ -2,10 +2,51 @@ const getNotes = require('./notes');
 const chalk = require('chalk');
 const yargs = require('yargs');
 
-const command = process.argv[2];
+yargs.version('1.1.0');
 
-if (command === 'add') {
-    console.log('addded');
-} else if (command === 'remove') {
-    console.log('removed');
-}
+yargs.command({
+    command: 'add',
+    describe: 'Add note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove note',
+    handler: function () {
+        console.log('Removing note');
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Print note',
+    handler: function () {
+        console.log('note');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function () {
+        console.log('notes');
+    }
+});
+
+yargs.parse();
