c585826baf2df733c3c10a26643e6e96350ac119
Luis Ramirez <sdaewa22@gmail.com>
2020-11-23 12:12:34 +0000

    chalk colors

--- notes.js
+++ notes.js
@@ -24,7 +24,7 @@ const addNote = function (title, body) {
         saveNotes(notes);
         console.log(chalk.green.inverse('Saved'));
     } else {
-        console.log(chalk.orange.inverse('Duplicate'));
+        console.log(chalk.yellow.inverse('Duplicate'));
     };
 }
 
@@ -61,7 +61,7 @@ const deleteNote = function (title) {
     } else if (notes.length === notesToKeep.length) {
         return console.log(chalk.red.inverse('Nothing happened'));
     } else if (notes.length === 0) {
-        console.log(chalk.orange.inverse('Empty'));
+        console.log(chalk.yellow.inverse('Empty'));
     }
 }
 
